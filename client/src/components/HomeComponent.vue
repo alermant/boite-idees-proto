b00287c57b976df5666e4ba204b48bc76ed1abe2
Unknown <yousri.tabbakh@gmail.com>
2019-02-27 23:27:55 +0100

    fix

--- client/src/components/HomeComponent.vue
+++ client/src/components/HomeComponent.vue
@@ -18,7 +18,7 @@
           <div
             class="soavis"
           >"Avec le brainstorm Bai, on fait du management visuel digitalisé. En un mois, nous avons résolu 80% de nos irritants."</div>
-          <div class="auteur">Jean-Baptiste Burin</div>
+          <div class="auteur">Jean-Baptiste Dupont</div>
           <div class="poste">Responsable de l’excellence Opérationnelle</div>
         </div>
       </div>
@@ -27,7 +27,7 @@
           <div
             class="soavis"
           >"D'un point de vue pédagogique, Bai apporte vraiment une interaction et une communication avec l'intégralité de la promotion."</div>
-          <div class="auteur">Clément Harnist</div>
+          <div class="auteur">Clément Dubois</div>
           <div class="poste">Responsable pédagogique</div>
         </div>
         <div class="picture"></div>
@@ -38,7 +38,7 @@
           <div
             class="soavis"
           >"Avec Bai, j'ai autant de plaisir à préparer les formations, qu'à les animer et qu'à les suivre ensuite."</div>
-          <div class="auteur">Dominique Rondot</div>
+          <div class="auteur">Dominique Dupuits</div>
           <div class="poste">Consultant indépendant</div>
         </div>
       </div>
